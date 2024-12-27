5e67c99e1ca0363b8e3b223ecdd8b4e854431d44
André Vieira <andre.g.vieira@gmail.com>
2024-12-27 23:42:13 +0000

    improved debug

--- src/airControlHandler.ts
+++ src/airControlHandler.ts
@@ -48,7 +48,7 @@ export abstract class AirControlHandler {
 
   async onStdErrData(error: string) {
     error = error.toString().replace(/\n$/, '');
-    this.platform.log.debug('onStdErrData():', error, this.accessory.displayName);    
+    this.platform.log.warn('onStdErrData():', error, this.accessory.displayName);
   }
 
   async onError(error: Error) {
